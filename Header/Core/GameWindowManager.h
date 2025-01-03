736ae0baf4ce754ecf4913a4672108300f88e521
Aman Kumar <aman.kumar01747@gmail.com>
2025-01-03 17:05:21 +0530

    -Customize game window

--- Header/Core/GameWindowManager.h
+++ Header/Core/GameWindowManager.h
@@ -1,2 +1,28 @@
 #pragma once
 #include "../../Header/Core/GameWindowManager.h"
+#include <SFML/Graphics.hpp>
+
+using namespace sf;
+
+namespace Core {
+    class GameWindowManager {
+    private:
+        int game_window_width = 1280;
+        int game_window_height = 720;
+
+        std::string game_title = "SFML-Pong";
+
+        RenderWindow* game_window;
+
+    public:
+        void createGameWindow();
+
+        RenderWindow* getGameWindow();
+
+        void initialize();
+
+        bool isGameRunning();
+
+        void render();
+    };
+}
