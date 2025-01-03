04930ee803713c331400d7c9754b31c0d531dc2c
Aman Kumar <aman.kumar01747@gmail.com>
2025-01-03 12:25:22 +0530

    -Create game window

---
+++ Source/Core/GameWindowManager.cpp
@@ -0,0 +1,32 @@
+#include "../../Header/Core/GameWindowManager.h"
+#include <iostream>
+
+
+class GameWindowManager {
+private:
+    int game_window_width = 1280;
+    int game_window_height = 720;
+
+public:
+
+    void initialize()
+    {
+
+    }
+
+    void createGameWindow()
+    {
+
+    }
+
+    bool isGameRunning()
+    {
+
+    }
+
+    void render()
+    {
+
+    }
+};
+
